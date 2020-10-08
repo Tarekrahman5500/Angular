ae277a08a4b60ae7513e7f5e4230ccea57bbfc97
Tarek Rahman <tr617739@gmail.com>
2020-10-09 01:41:57 +0600

    Header and Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,11 +16,15 @@ import {MatLineModule} from '@angular/material/core';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 import { DishService } from './services/dish.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
