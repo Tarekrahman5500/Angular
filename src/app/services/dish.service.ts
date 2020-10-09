83afa96680296f10fcb7ff2b3ea8b536ea437268
Tarek Rahman <tr617739@gmail.com>
2020-10-09 22:33:10 +0600

    Single Page Applications Part 1

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -1,15 +1,26 @@
-import { Injectable } from '@angular/core';
-import { Dish} from '../shared /dish';
-import { DISHES} from '../shared /dishes';
+import {Injectable} from '@angular/core';
+import {Dish} from '../shared /dish';
+import {DISHES} from '../shared /dishes';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DishService {
 
-  constructor() { }
+  constructor() {
+  }
 
   getDishes(): Dish[] {
-    return  DISHES;
+    return DISHES;
+  }
+
+  getDish(id: string): Dish {
+    return DISHES.filter((dish) => (dish.id === id))[0];
+  }
+
+  getFeaturedDish(): Dish {
+    return DISHES.filter((dish) => dish.featured)[0];
   }
 }
+
+
